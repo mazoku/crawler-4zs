be0644401f093c89f5fde262bad8d054af64e313
Tomas Ryba <rybato@gmail.com>
2018-04-17 15:51:27 +0200

    Formatovani sheetu.

--- crawler4zs/spreadsheetator.py
+++ crawler4zs/spreadsheetator.py
@@ -21,16 +21,21 @@ class Spreadsheetator:
         # get worksheets
         self.trida_ws = self.sheet.worksheet('trida')
 
+    def append(self, data):
+        self.trida_ws.insert_row(data, 2, 'USER_ENTERED')
+
     def read_all(self):
         # list data in first column
         # values_list = self.trida_ws.col_values(1)
         # print(values_list)
 
-        data = self.trida_ws.get_all_values()
+        data = self.trida_ws.get_all_values()[1:]
         print(data)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     stator = Spreadsheetator()
+    stator.read_all()
+    stator.append(['18.4.2018', 'Nadpis - insert test', 'odkaz - insert test', 'obsah - insert test'])
     stator.read_all()
